14189b2cc9ca4f48e948d605285114bd03ab5566
Ben Hemsworth <Ben_hemsworth@hotmail.co.uk>
2020-05-11 12:20:40 +0100

    create gameMaster file

---
+++ source/gameMaster.py
@@ -0,0 +1,8 @@
+##
+##  Isolation Pong game master file.
+##  @version 0.01
+##  @author Ben Hemsworth <ben_hemsworth@hotmail.co.uk>
+
+## this script should hold every top level object to run the game
+## DO NOT put your test scripts in here
+## this file should run the current version of the game when executed
