4818671e762747b19a0d70c97c572c392f2046e0
Ben Hemsworth <Ben_hemsworth@hotmail.co.uk>
2020-05-14 20:33:10 +0100

    create glProgram

--- source/gl-handler.py
+++ source/gl-handler.py
@@ -3,15 +3,20 @@
 ##  @version 0.01
 ##  @author Ben Hemsworth <ben_hemsworth@hotmail.co.uk>
 
+import pathlib
+
 from OpenGL.GL import *
-from OpenGL.GLU import *
+##from OpenGL.GLU import *
 import glfw
 
 
 def load_shader(shader_file, GL_SHADER_TYPE):
     shader_ID = glCreateShader(GL_SHADER_TYPE)
 
-    file = open(shader_file, 'r')
+    ## Load shaders in same folder as this file
+    shader_file_location = pathlib.Path(__file__).parent.absolute() / shader_file
+
+    file = open(shader_file_location, 'r')
     shader_source = file.read()
 
     glShaderSource(shader_ID, shader_source)
@@ -27,13 +32,19 @@ def draw_screen():
 
 
 def setup():
-    glfw.init()
 
+    glfw.init()
     glfw.window_hint(glfw.DOUBLEBUFFER, glfw.TRUE)
     window = glfw.create_window(800,600, "Isolation Pong", None, None)
-
     glfw.make_context_current(window)
     glfw.swap_interval(2)
+
+
+    program_ID = glCreateProgram();
+    glAttachShader(program_ID, load_shader('./simple.frag', GL_FRAGMENT_SHADER))
+    glAttachShader(program_ID, load_shader('./simple.vert', GL_VERTEX_SHADER))
+    glLinkProgram(program_ID)
+
 
     while not glfw.window_should_close(window):
         draw_screen()
